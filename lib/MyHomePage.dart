a01341c5dd3bea3b20df6b7984f61dd76bf124ac
sounishnath003 <flock.sinasini@gmail.com>
2019-06-24 08:59:39 +0530

    future function

--- lib/MyHomePage.dart
+++ lib/MyHomePage.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart'as http;
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key}) : super(key: key);
@@ -7,6 +10,19 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+
+
+  Future<void> fetchData() async {
+    final url = await http.get("https://opentdb.com/api.php?amount=30");
+    if (url.statusCode == 200) {
+      var decodeRes = jsonDecode(url.body) ;
+      
+    } else {
+      throw Exception("Failed to load, try again later");
+    }
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -17,13 +33,7 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Colors.white,
         elevation: 1.0,
       ),
-      body: _appBody(),
+      body: null,
     );
   }
-}
-
-Widget _appBody() {
-  return new Center(
-    child: new Text("hey Bro"),
-  );
 }
